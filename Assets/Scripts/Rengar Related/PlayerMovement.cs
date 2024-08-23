a09fcf7565fbf804ebaaac44fb6e0e4e87bf08d8
Saksham Kumar <sakshams21@gmail.com>
2024-08-23 17:37:46 +0530

    some changes

--- Assets/Scripts/Rengar Related/PlayerMovement.cs
+++ Assets/Scripts/Rengar Related/PlayerMovement.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 2f;
     public float HeightMultiplier = 2f;
+    public NavMeshAgent MeshAgent;
     public AnimationCurve HeightCurve;
+    public GameObject RangeIndicator_GameObject;
 
     private Vector3 _start;
     private Vector3 _destination;
@@ -16,7 +19,22 @@ public class PlayerMovement : MonoBehaviour
     private float _duration= 0;
 
     private bool _initiate=false;
+    private bool _leapStarted;
+    private bool _isPlayerInBush;
 
+    private void Start()
+    {
+        _isPlayerInBush = false;
+        
+        //MeshAgent.autoRepath = true;
+    }
+
+    private void Update() => NormalMovement();
+
+    private bool CheckForBush()
+    {
+        return _isPlayerInBush;
+    }
     public void AssignDestination(Vector3 destinationValue)
     {
         _destination = destinationValue;
@@ -26,12 +44,19 @@ public class PlayerMovement : MonoBehaviour
         _time = 0;
     }
 
-
-    private void Update()
+    private void NormalMovement()
     {
         if (!_initiate)
             return;
         
+        MeshAgent.SetDestination(_destination);
+    }
+
+    private void BushMovement()
+    {
+        if (!_initiate)
+            return;
+        _leapStarted = true;
         _time += Time.deltaTime;
         float a = _time / _duration;
         if (a < 1f)
@@ -42,6 +67,25 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             _initiate = false;
+            _leapStarted = false;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RangeIndicator_GameObject.SetActive(other.gameObject.TryGetComponent(out Bush _));
+        _isPlayerInBush = other.gameObject.TryGetComponent(out Bush _);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(RangeIndicator_GameObject.activeSelf)
+            RangeIndicator_GameObject.SetActive(false);
+        _isPlayerInBush = false;
+    }
+}
+
+public enum MoveType
+{
+    Leap, Move, MovementIncludingLeap
 }
